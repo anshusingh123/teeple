04da6cdabb2a7eaebd5fa5b556c3fc6b242bed6d
youngmin kim <youngmin.k@gmail.com>
2011-07-08 15:24:04 +0900

    upload_contacts와 audience_list수정 중

--- delete_comment.php
+++ delete_comment.php
@@ -5,10 +5,17 @@
  * @file
  * Handles incoming requests to fire off regularly-scheduled tasks (cron jobs).
  */
-
 include_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-comment_delete(1514);
+module_load_include('inc', 'comment', 'comment.admin');
+
+
+$query = "SELECT cid FROM comments WHERE uid=1";
+$result  = db_query($query);
+
+while ($c = db_fetch_array($result)) {
+    comment_delete($c['cid']);
+}
 
 ?>
